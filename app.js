f939a259a0d6499cfaa88815fde70d00545def5e
Alena <alena.agapova.01@bk.ru>
2026-04-23 16:40:29 +0300

    fix code

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-const canDrive = (age >= 18 && hasLicence && !isDrunk) ? 'может' : 'не может';
+const age = 18, hasLicence = true, isDrunk = false;
+const result = (age >= 18 && hasLicence && !isDrunk) ? 'может' : 'не может';
